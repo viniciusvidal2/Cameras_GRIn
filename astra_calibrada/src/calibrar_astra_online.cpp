9c1b2c8b6d52420cea1f429544bc6771f74f4fb5
Vinicius <vinicius.vidal@engenharia.ufjf.br>
2019-05-20 19:49:41 -0300

    ajustando captura de dados da rgbd

--- astra_calibrada/src/calibrar_astra_online.cpp
+++ astra_calibrada/src/calibrar_astra_online.cpp
@@ -47,61 +47,86 @@ using namespace tf;
 using namespace message_filters;
 using namespace nav_msgs;
 
+bool primeira_vez = true;
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Variaveis e definicoes globais
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Definicoes
 typedef PointXYZRGB       PointT;
-typedef sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::PointCloud2> syncPolicy;
+typedef sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::PointCloud2> syncPolicy;
 
-void callback(const sensor_msgs::ImageConstPtr& msg_rgb,
-              const sensor_msgs::ImageConstPtr& msg_depth,
+void callback(const sensor_msgs::ImageConstPtr& msg_img,
               const sensor_msgs::PointCloud2ConstPtr& cloud){
-    cv_bridge::CvImagePtr cv_ptr_d;
-    cv_bridge::CvImagePtr cv_ptr_rgb;
-    cv_ptr_d   = cv_bridge::toCvCopy(msg_depth, sensor_msgs::image_encodings::TYPE_16UC1);
-    cv_ptr_rgb = cv_bridge::toCvCopy(msg_rgb,   sensor_msgs::image_encodings::TYPE_8UC3 );
+    cv_bridge::CvImagePtr cv_ptr_img;
+    cv_ptr_img = cv_bridge::toCvCopy(msg_img, sensor_msgs::image_encodings::TYPE_8UC3 );
 
-    cv::Mat image_gray;
-    cv::cvtColor(cv_ptr_rgb->image, image_gray, cv::COLOR_BGR2GRAY);
+    if(primeira_vez){
 
-    cv::imwrite("/home/grin/Desktop/calibrando/gray.jpg", image_gray);
-    cv::imwrite("/home/grin/Desktop/calibrando/rgb.jpg" , cv_ptr_rgb->image);
-    cv::imwrite("/home/grin/Desktop/calibrando/ir.jpg"  , cv_ptr_d->image);
+        ROS_INFO("Gravando a nuvem de pontos e imagem colorida");
 
-    PointCloud<PointT>::Ptr xyzrgb (new PointCloud<PointT>());
-    fromROSMsg(*cloud, *xyzrgb);
-    vector<int> indices;
-    removeNaNFromPointCloud(*xyzrgb, indices);
-    PointCloud<PointXYZ>::Ptr xyz (new PointCloud<PointXYZ>());
-    for(int i=0; i < xyzrgb->size(); i++){
-        PointXYZ ponto;
-        ponto.x = xyzrgb->points[i].x;
-        ponto.y = xyzrgb->points[i].y;
-        ponto.z = xyzrgb->points[i].z;
+        cv::Mat image_gray;
+        cv::cvtColor(cv_ptr_img->image, image_gray, cv::COLOR_BGR2GRAY);
 
-        xyz->push_back(ponto);
+        cv::imwrite("/home/grin/Desktop/calibrando/gray.jpg", image_gray);
+        cv::imwrite("/home/grin/Desktop/calibrando/rgb.jpg" , cv_ptr_img->image);
+
+        PointCloud<PointT>::Ptr xyzrgb (new PointCloud<PointT>());
+        fromROSMsg(*cloud, *xyzrgb);
+        vector<int> indices;
+        removeNaNFromPointCloud(*xyzrgb, *xyzrgb, indices);
+
+        PointCloud<PointXYZ>::Ptr xyz (new PointCloud<PointXYZ>());
+        for(int i=0; i < xyzrgb->size(); i++){
+            PointXYZ ponto;
+            ponto.x = xyzrgb->points[i].x;
+            ponto.y = xyzrgb->points[i].y;
+            ponto.z = xyzrgb->points[i].z;
+
+            xyz->push_back(ponto);
+        }
+
+        ROS_INFO("Gravando nuvem...");
+
+        pcl::io::savePLYFileASCII("/home/grin/Desktop/calibrando/nuvem.ply", *xyz);
+        pcl::io::savePLYFileASCII("/home/grin/Desktop/calibrando/nuvem_total.ply", *xyzrgb);
+
+        primeira_vez = false;
+
+    } else {
+
+        ROS_INFO("Gravando imagem infravermelha...");
+
+        cv::imwrite("/home/grin/Desktop/calibrando/ir.jpg", cv_ptr_img->image);
+
+        ROS_INFO("TUDO GRAVADO");
+
+        ros::shutdown();
     }
-    pcl::io::savePLYFileASCII("/home/grin/Desktop/calibrando/nuvem.ply", *xyz);
-    pcl::io::savePLYFileASCII("/home/grin/Desktop/calibrando/nuvem_total.ply", *xyzrgb);
-    ros::shutdown();
+
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "calibrar_astra_online");
-  ros::NodeHandle nh;
+    ros::init(argc, argv, "calibrar_astra_online");
+    ros::NodeHandle nh;
 
-  message_filters::Subscriber<sensor_msgs::Image>       rgb_sub  (nh, "/camera/rgb/image_raw"          , 10);
-  message_filters::Subscriber<sensor_msgs::Image>       depth_sub(nh, "/camera/depth/image_raw"        , 10);
-  message_filters::Subscriber<sensor_msgs::PointCloud2> cloudsub (nh, "/camera/depth_registered/points", 10);
-  Synchronizer<syncPolicy> sync(syncPolicy(10), rgb_sub, depth_sub, cloudsub);
-  sync.registerCallback(boost::bind(&callback, _1, _2, _3));
+    message_filters::Subscriber<sensor_msgs::Image>       img_sub  (nh, "/camera/rgb/image_raw"          , 10);
+    message_filters::Subscriber<sensor_msgs::PointCloud2> cloudsub (nh, "/camera/depth_registered/points", 10);
+    Synchronizer<syncPolicy> sync(syncPolicy(10), img_sub, cloudsub);
+    sync.registerCallback(boost::bind(&callback, _1, _2));
 
 
-  while(ros::ok()){
-      ros::spinOnce();
-  }
+    while(ros::ok()){
+        if(primeira_vez == false){
+//            img_sub.unsubscribe();
+//            cloudsub.unsubscribe();
+
+            img_sub.subscribe( nh, "/camera/ir/image_raw"   , 10);
+            cloudsub.subscribe(nh, "/camera/depth/image_raw", 10);
+        }
+        ros::spinOnce();
+    }
 
 }
